c073e080bd86542bea0d45fd016730d96b750e61
Angelique Abacajan <ma_utsey@yahoo.com>
2022-05-31 09:21:00 -0400

    Implemented functionality to store notes to local storage

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo));
+}
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
 
@@ -26,18 +32,36 @@ function addTodo(todo) {
 
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
+            updateLocalStorage();
         }) 
 
         todoEl.addEventListener("contextmenu", (event) => {
             event.preventDefault();
 
             todoEl.remove();
+
+            updateLocalStorage();
         }) 
 
         todosUL.appendChild(todoEl);
 
         input.value = "";
 
+        updateLocalStorage();
     }
 }
 
+function updateLocalStorage() {
+    todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed")
+        })
+    })
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
